f5f1f9fbe248bf578e3206b161c6c755a460e614
imRaja001 <khalidhashimmth0123@gmail.com>
2026-08-19 23:16:07 +0530

    Add reusable login test setup

--- tests/ui/login.spec.ts
+++ tests/ui/login.spec.ts
@@ -1,9 +1,20 @@
 import { test, expect } from '../../src/fixtures/test.fixture';
 import users from '../../test-data/json/users.json';
 
-test('successful login', async ({ page, loginPage }) => {
-    await loginPage.navigateTo();
-    await loginPage.login(users.validUser.username, users.validUser.password);
-    await expect(page.getByText("Swag Labs")).toBeVisible();
 
+test.beforeEach(async ({ loginPage }) => {
+    await loginPage.navigateTo();
+
+    await loginPage.login(
+        users.validUser.username,
+        users.validUser.password
+    );
+});
+
+test('successful login', async ({ page }) => {
+    await expect(page.getByText("Swag Labs")).toBeVisible();
+});
+
+test('user is redirected to inventory page after login', async ({ page }) =>{
+    await expect(page).toHaveURL('/inventory\.html/');
 });
